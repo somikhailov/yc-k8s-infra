6f8885d7053ed5f4f0c85e0ca0c27c11948a7b3d
Sergei Mikhailov <somikhaylov@yandex.ru>
2022-05-04 20:44:28 +0700

    add tf yc-kubeconfig

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ module "yc-k8s-infra" {
   yc_folder_id = var.yc_folder_id
   yc_zone      = "ru-central1-a"
 }
+
+resource "null_resource" "yc-kubeconfig" {
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<-EOT
+      yc managed-kubernetes cluster get-credentials --id ${module.yc-k8s-infra.k8s_cluster_id} --external --force
+    EOT
+  }
+}
